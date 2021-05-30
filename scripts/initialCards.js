33558fd41745f2ba4f8d78ac402020a15142d552
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-05-31 00:05:33 +0300

    Начал JS. 1 попап

---
+++ scripts/initialCards.js
@@ -0,0 +1,49 @@
+const initialCards = [{
+  name: 'Шашлык',
+  link: 'https://im0-tub-ru.yandex.net/i?id=6778eaec4d497c0e014eee55e8459624-l&n=13'
+},
+{
+  name: 'Плов',
+  link: 'https://img-fotki.yandex.ru/get/5645/11206178.2ed/0_cd32a_d1922c64_orig'
+},
+{
+  name: 'Долма',
+  link: 'https://im0-tub-ru.yandex.net/i?id=3131fb9213a355557bf8d9ea8935a0ea-l&n=13'
+},
+{
+  name: 'Баранина запечённая',
+  link: 'https://avatars.mds.yandex.net/get-zen_doc/51478/pub_5b335a04b151b800aa2a1620_5b335a56146cd600a920e2a6/scale_1200'
+},
+{
+  name: 'Утка по-пекински',
+  link: 'https://bankety.tasty-family.ru/img/menu/горячее/Уткапо-пекински.jpg'
+},
+{
+  name: 'Хачапури',
+  link: 'https://chaihanadastarhan.ru/upload/corvax.mini/6b6/6b616c3f1d3a6fc7e6f4a06f080afcaa.png'
+},
+{
+  name: 'Борщ',
+  link: 'https://i.pinimg.com/736x/f2/2a/5f/f22a5f4cb44baeaaed79fa12c3401f13.jpg'
+},
+{
+  name: 'Рассольник',
+  link: 'https://bankety.tasty-family.ru/img/menu/горячее/Уткапо-пекински.jpg'
+},
+{
+  name: 'Блины с творогом',
+  link: 'http://bookandcook.pl/uploads/recipe/0eecbfa323f55ad7b2858159ed6ad4a623689725.jpg'
+},
+{
+  name: 'Халва',
+  link: 'https://i.pinimg.com/736x/23/57/db/2357db5ee5df1a1fc6119a66a1aacc9d--romanian-recipes-romanian-desserts.jpg'
+},
+{
+  name: 'Наполеон',
+  link: 'https://mykaleidoscope.ru/uploads/posts/2020-01/1579919107_10-p-torti-napoleon-15.jpg'
+},
+{
+  name: 'Лимонный морс',
+  link: 'https://i.pinimg.com/originals/0c/88/97/0c8897da2e823db5b1728a8d955e8a4f.png'
+},
+];
